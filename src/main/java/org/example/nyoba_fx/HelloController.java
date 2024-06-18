c669b2677ff369b4bb637756ef9f6f47ec60da94
MayorHarist <malfatih503@gmail.com>
2024-06-18 13:48:56 +0700

    sabar

--- src/main/java/org/example/nyoba_fx/HelloController.java
+++ src/main/java/org/example/nyoba_fx/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Woi kerjain!!!");
+        welcomeText.setText("ini gw lagi ngerjain suuu");
     }
 }
