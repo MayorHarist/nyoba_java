b04f7f3e603d1dfcae2745e337475f93bdcb4ce8
MayorHarist <malfatih503@gmail.com>
2024-06-18 13:39:54 +0700

    commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module org.example.nyoba_fx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.dlsc.formsfx;
+
+    opens org.example.nyoba_fx to javafx.fxml;
+    exports org.example.nyoba_fx;
+}
